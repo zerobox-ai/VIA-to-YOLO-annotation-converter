37ba20d6e9d716d98f105a18cad8e2efdd7b548d
Leo Ying <leo.ying@boardee.com>
2021-09-24 22:08:13 -0600

    Fixed some bugs

--- via2dark.py
+++ via2dark.py
@@ -1,38 +1,43 @@
+import ssl
 import sys
 import json
 from PIL import Image
 from decimal import *
 
 def get_object_class(region, file, names):
-    try:        
-        type = region['region_attributes']['Type']
-        package = region['region_attributes']['package']
+    try:
+        type = region['region_attributes']['type']
+        # package = region['region_attributes']['package']
     except KeyError:
         print >> sys.stderr, "type or package info is missing in ", file
 
-    name = type + " " + package
+    name = type
     index = [item.lower() for item in names].index(name.lower())
-    
+
     return index
 
+
 def get_dark_annotation(region, size):
-        x = region['shape_attributes']['x']
-        y = region['shape_attributes']['y']
-        width = region['shape_attributes']['width']
-        height = region['shape_attributes']['height']
+    x = region['shape_attributes']['x']
+    y = region['shape_attributes']['y']
+    width = region['shape_attributes']['width']
+    height = region['shape_attributes']['height']
 
-        _x      = Decimal(x + width) / Decimal(2 * size[0]) # relative position of center x of rect
-        _y      = Decimal(y + height) / Decimal(2 * size[1]) # relative position of center y of rect
-        _width  = Decimal(width / size[0])
-        _height = Decimal(height / size[1])
+    _x = Decimal(x + width) / Decimal(2 * size[0])  # relative position of center x of rect
+    _y = Decimal(y + height) / Decimal(2 * size[1])  # relative position of center y of rect
+    _width = Decimal(width / size[0])
+    _height = Decimal(height / size[1])
 
-        return "{0:.10f} {0:.10f} {0:.10f} {0:.10f}".format(_x, _y, _width, _height)
+    return "{0:.10f} {0:.10f} {0:.10f} {0:.10f}".format(_x, _y, _width, _height)
+
 
 def main():
     with open(sys.argv[1:][0]) as file:
         dict = json.load(file)
-        
-        try:        
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        try:
             namesFile = sys.argv[1:][1]
             names = open(namesFile).read().split('\n')
         except IndexError:
@@ -42,14 +47,18 @@ def main():
             data = dict[key]
 
             imageName = data['filename']
-            filename = imageName.rsplit('.', 1)[0]
-            
+            filename = imageName.rsplit('/')[-1]
+
             regions = data['regions']
 
-            try:        
-                img = Image.open(imageName)
+            try:
+                import urllib.request
+
+                urllib.request.urlretrieve(imageName, filename)
+                img = Image.open(filename)
+
             except IOError:
-                print >> sys.stderr, "No such file" , imageName
+                print(sys.stderr + "No such file" + imageName)
 
             content = ""
             for region in regions:
@@ -59,6 +68,7 @@ def main():
 
             with open("{}.txt".format(filename), "w") as outFile:
                 outFile.write(content)
+
 
 if __name__ == "__main__":
     main()
